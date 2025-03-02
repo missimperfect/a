cbee170ba102b859179b2680345a010772d6c4e7
missimperfect <40125698+missimperfect@users.noreply.github.com>
2025-03-02 19:27:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-(function($){
-    $(document).ready(function(){
-        $("[title]").style_my_tooltips({
-            tip_follows_cursor:true,
-            tip_delay_time:200,
-            tip_fade_speed:300
-        });
-    });
-})(jQuery);
 
-$(document).ready(function(){
-    $(".droptags").hide();
-    $(".droptitle").click(function(){
-        $(this).next(".droptags").slideToggle('fast');
-    });
-});
